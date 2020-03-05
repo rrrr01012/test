69a7b183be9bff5709d00a58ea6e478e283b5ea6
red <rrrr01012@gmail.com>
2020-03-05 22:07:44 +0800

    done

--- js_course/app.js
+++ js_course/app.js
@@ -1,9 +1,14 @@
 ﻿b();
-console.log(a);
+console.log(a);         //變數會預設undefine
 
-var a= 'hello world';
-
+var a;          //mem中載入一個undefined變數a
+if(a===undefined){
+    console.log("a is undefined .");
+}
+else{
+    console.log("a is defined .");
+}
 
 function b(){
-    console.log('Call b!');
+    console.log('Call b!');  //js的function code會預先載入mem中
 }
