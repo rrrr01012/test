7b2bc701227f5438209b7b80278f4ed540e8a42c
unknown <rrrr01012@gmail.com>
2020-05-15 21:24:17 +0800

    dine

---
+++ node_course/05/01.js
@@ -0,0 +1,17 @@
+ function add(x,y,callback){
+    console.log(1)
+    setTimeout(() => {
+        console.log(2)
+        var ret = x + y
+        callback(ret)
+    }, 1000);
+ }
+function res(ret){
+    console.log(ret)
+}
+ add(20,10,res)
+//  add(20,10,function res(ret){
+//      console.log(ret)
+//  })
+ console.log(3)
+
